ec8645dd323a34415ebf75144bedcad21f705096
Neïl Aloulou <98472708+JoyfulCanis@users.noreply.github.com>
2023-04-22 17:09:41 +0200

    user.js (x3) done. Missing AUTH/SIGNUP AUTH/LOGIN !!!

--- app.js
+++ app.js
@@ -2,9 +2,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+//---------[DEBUT]-------Utilisation unique, à supprimer ensuite-----[DEBUT]-------------
+const crypto = require("crypto");
+const SECRET_KEY = crypto.randomBytes(64).toString("hex");
+//Chargement de la variable d'environnement
+require("dotenv").config();
+console.log("TS IS", SECRET_KEY);
+//----------[FIN]------Utilisation unique, à supprimer ensuite-------[FIN]-----------
+
 const app = express();
 
-//Connect mongoose
+//Connect to Mongoose
 mongoose
   .connect(
     "mongodb+srv://johndoe:admin@piiquante.aeuww2a.mongodb.net/?retryWrites=true&w=majority",
