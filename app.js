2e7ccc1275afca45b478b65437fdffddd6562c1a
Neïl Aloulou <98472708+JoyfulCanis@users.noreply.github.com>
2023-04-24 18:05:26 +0200

    sauce model and controller created + auth.js + multe-config.js

--- app.js
+++ app.js
@@ -23,4 +23,33 @@ mongoose
 
 app.use(express.json());
 
+//Middleware permettant d'éviter les conflits CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = app;
